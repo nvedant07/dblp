1cbb09056d62b9b9999d3c38df4e212e1dd1376c
Arpan Mondal <Razor@Arpans-MacBook-Air.local>
2016-11-05 20:28:43 +0530

    added main frame

--- GUI.java
+++ GUI.java
@@ -5,5 +5,7 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GUI{
-	
+	JFrame mainframe = new JFrame("Main Frame");
+	mainframe.setSize(640, 480);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
