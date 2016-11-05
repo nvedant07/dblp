97f2d7ce0e6db7002230c26d50e14c28e15d6130
Arpan Mondal <Razor@Arpans-MacBook-Air.local>
2016-11-05 21:06:33 +0530

    GUI: fixing mistakes

--- GUI.java
+++ GUI.java
@@ -5,7 +5,10 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GUI{
-	JFrame mainframe = new JFrame("Main Frame");
-	mainframe.setSize(640, 480);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private JFrame mainFrame;
+	GUI(){
+		JFrame mainframe = new JFrame("Main Frame");
+		mainframe.setSize(640, 480);
+    	mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
