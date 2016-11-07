610c78783fd2e7bf3488bb45edf8cdc54b47af70
Arpan Mondal <Razor@Arpans-MacBook-Air.local>
2016-11-07 16:20:12 +0530

    added buttons

--- GUI.java
+++ GUI.java
@@ -19,7 +19,12 @@ public class GUI extends JFrame{
 		label1.setAlignmentX(startPanel.CENTER_ALIGNMENT);
 		//label1.setText("Tic Tac Toe");
 		startPanel.add(label1);
-
+		JButton button1 = new JButton("Start Game");
+		button1.setAlignmentX(startPanel.CENTER_ALIGNMENT);
+		startPanel.add(button1);
+		JButton button2 = new JButton("Exit");
+		button2.setAlignmentX(startPanel.CENTER_ALIGNMENT);
+		startPanel.add(button2);
 		this.add(startPanel);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("VedArpan Project");
