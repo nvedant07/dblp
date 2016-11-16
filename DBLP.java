792d7bfb56686589972396751d04b2d8d8f9bad3
Vedant Nanda <vedant15114@iiitd.ac.in>
2016-11-16 19:20:34 +0530

    made menu driven engine

--- DBLP.java
+++ DBLP.java
@@ -7,77 +7,43 @@ public class DBLP {
 	static String author_to_search;
 	static List<List<String>> same_names=new ArrayList<List<String>>();
 	
-	public void print_authors(ArrayList<String> arr){
-		for(int j=0;j<arr.size();j++){
-			if(j==arr.size()-1){
-				System.out.print(arr.get(j));
-			}
-			else{
-				System.out.print(arr.get(j)+",");
-			}
-			
-		}
-	}
-	
 	public static void main(String[] args) {
 		//parse www records
 		WwwParser www=new WwwParser("dblp.xml");
-//		System.out.println(Person.same_names.size());
-//		System.out.println(same_names.get(1).toString());
-		//parse and search for string in other records
+		//to be moved to GUI
 		Scanner in=new Scanner(System.in);
-		System.out.println("Enter author name");
-		String author=in.nextLine();
-		author_to_search=author;
-		CustomParser p = new CustomParser("dblp.xml",author);
-		//print the result
-		DBLP dblp=new DBLP();
-		for(int i=0;i<result_publications.size();i++){
-			System.out.print((i+1)+" | ");
-			dblp.print_authors(result_publications.get(i).getAuthors());
-			if(result_publications.get(i).getTitle()!=null){
-				System.out.print(" | "+result_publications.get(i).getTitle());
+		while(true){
+			System.out.print("Choose an option:\n1.Query1\n2.Query2\n3.Query3\n4.exit\n");
+			int opt=in.nextInt();
+			if(opt==4)break;
+			else if(opt==1){
+				Query_1 q=new Query_1();
+				System.out.print("1.Search by authorname\n2.Search by title\n");
+				opt=in.nextInt();
+				if(opt==1){
+					q.setSearch_by_authorname(true);
+					q.setSearch_by_title(false);
+					System.out.print("1.Sort by date\n2.Since some year\n3.Between two years\n");
+					opt=in.nextInt();
+					if(opt==1){
+						q.setSort_by_date(true);
+						q.setBw_two_years(false);
+						q.setSince_some_year(false);
+					}
+					else if(opt==2){
+						q.setSince_some_year(true);
+						q.setSort_by_date(false);
+						q.setBw_two_years(false);
+					}
+					else if(opt==3){
+						q.setBw_two_years(true);
+						q.setSince_some_year(false);
+						q.setSort_by_date(false);
+					}
+					
+				}
+				q.return_query();
 			}
-			else{
-				System.out.print(" | NO TITLE");
-			}
-			if(result_publications.get(i).getPages()!=null){
-				System.out.print(" | "+result_publications.get(i).getPages());
-			}
-			else{
-				System.out.print(" | NO PAGES");
-			}
-			if(result_publications.get(i).getYear()!=0){
-				System.out.print(" | "+result_publications.get(i).getYear());
-			}
-			else{
-				System.out.print(" | NO YEAR");
-			}
-			if(result_publications.get(i).getVolume()!=null){
-				System.out.print(" | "+result_publications.get(i).getVolume());
-			}
-			else{
-				System.out.print(" | NO VOLUME");
-			}
-			if(result_publications.get(i).getJournal()!=null){
-				System.out.print(" | "+result_publications.get(i).getJournal());
-			}
-			else{
-				System.out.print(" | NO JOURNAL");
-			}
-			if(result_publications.get(i).getBooktitle()!=null){
-				System.out.print(" | "+result_publications.get(i).getBooktitle());
-			}
-			else{
-				System.out.print(" | NO BOOKTITLE");
-			}
-			if(result_publications.get(i).getUrl()!=null){
-				System.out.println(" | "+result_publications.get(i).getUrl());
-			}
-			else{
-				System.out.println(" | NO URL");
-			}
-			
 		}
 	}
 
---
+++ Query_1.java
@@ -0,0 +1,133 @@
+package dblp;
+
+import java.util.*;
+
+public class Query_1 {
+	private boolean sort_by_date;
+	private boolean sort_by_relevance;
+	private boolean search_by_title;
+	private boolean search_by_authorname;
+	private boolean since_some_year;
+	private int year;
+	private boolean bw_two_years;
+	private int start_year;
+	private int end_year;
+	public boolean isSort_by_date() {
+		return sort_by_date;
+	}
+	public void setSort_by_date(boolean sort_by_date) {
+		this.sort_by_date = sort_by_date;
+	}
+	public boolean isSort_by_relevance() {
+		return sort_by_relevance;
+	}
+	public void setSort_by_relevance(boolean sort_by_relevance) {
+		this.sort_by_relevance = sort_by_relevance;
+	}
+	public boolean isSearch_by_title() {
+		return search_by_title;
+	}
+	public void setSearch_by_title(boolean search_by_title) {
+		this.search_by_title = search_by_title;
+	}
+	public boolean isSearch_by_authorname() {
+		return search_by_authorname;
+	}
+	public void setSearch_by_authorname(boolean search_by_authorname) {
+		this.search_by_authorname = search_by_authorname;
+	}
+	public boolean isSince_some_year() {
+		return since_some_year;
+	}
+	public void setSince_some_year(boolean since_some_year) {
+		this.since_some_year = since_some_year;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public boolean isBw_two_years() {
+		return bw_two_years;
+	}
+	public void setBw_two_years(boolean bw_two_years) {
+		this.bw_two_years = bw_two_years;
+	}
+	public int getStart_year() {
+		return start_year;
+	}
+	public void setStart_year(int start_year) {
+		this.start_year = start_year;
+	}
+	public int getEnd_year() {
+		return end_year;
+	}
+	public void setEnd_year(int end_year) {
+		this.end_year = end_year;
+	}
+	public void print_authors(ArrayList<String> arr){
+		for(int j=0;j<arr.size();j++){
+			if(j==arr.size()-1){
+				System.out.print(arr.get(j));
+			}
+			else{
+				System.out.print(arr.get(j)+",");
+			}
+			
+		}
+	}
+	public void return_query(){
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter author name");
+		String author=in.nextLine();
+		DBLP.author_to_search=author;
+		CustomParser p = new CustomParser("dblp.xml",author);
+		for(int i=0;i<DBLP.result_publications.size();i++){
+			System.out.print((i+1)+" | ");
+			this.print_authors(DBLP.result_publications.get(i).getAuthors());
+			if(DBLP.result_publications.get(i).getTitle()!=null){
+				System.out.print(" | "+DBLP.result_publications.get(i).getTitle());
+			}
+			else{
+				System.out.print(" | NO TITLE");
+			}
+			if(DBLP.result_publications.get(i).getPages()!=null){
+				System.out.print(" | "+DBLP.result_publications.get(i).getPages());
+			}
+			else{
+				System.out.print(" | NO PAGES");
+			}
+			if(DBLP.result_publications.get(i).getYear()!=0){
+				System.out.print(" | "+DBLP.result_publications.get(i).getYear());
+			}
+			else{
+				System.out.print(" | NO YEAR");
+			}
+			if(DBLP.result_publications.get(i).getVolume()!=null){
+				System.out.print(" | "+DBLP.result_publications.get(i).getVolume());
+			}
+			else{
+				System.out.print(" | NO VOLUME");
+			}
+			if(DBLP.result_publications.get(i).getJournal()!=null){
+				System.out.print(" | "+DBLP.result_publications.get(i).getJournal());
+			}
+			else{
+				System.out.print(" | NO JOURNAL");
+			}
+			if(DBLP.result_publications.get(i).getBooktitle()!=null){
+				System.out.print(" | "+DBLP.result_publications.get(i).getBooktitle());
+			}
+			else{
+				System.out.print(" | NO BOOKTITLE");
+			}
+			if(DBLP.result_publications.get(i).getUrl()!=null){
+				System.out.println(" | "+DBLP.result_publications.get(i).getUrl());
+			}
+			else{
+				System.out.println(" | NO URL");
+			}	
+		}
+	}
+}
